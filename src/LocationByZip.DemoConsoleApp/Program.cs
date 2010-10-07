d9d63c4647d416c63ea71368e1f84e12f2985a7f
Jon Sagara <devnull@localhost>
2010-10-07 10:37:09 -0700

    Reformatted the demo app's output to make it more clear what is going on.

--- src/LocationByZip.DemoConsoleApp/Program.cs
+++ src/LocationByZip.DemoConsoleApp/Program.cs
@@ -11,8 +11,8 @@ namespace LocationByZip.DemoConsoleApp
 		{
 			ILocationService locationSvc = new LocationService();
 
-
-			//	Location by ZIP Code.
+			// Location by ZIP Code
+			Console.WriteLine("=== Location by ZIP Code ===");
 			Location location = locationSvc.GetByZipCode("93275");
 			if (location != null)
 			{
@@ -20,7 +20,8 @@ namespace LocationByZip.DemoConsoleApp
 			}
 
 
-			//	Location(s) by City/State.
+			// Location(s) by City/State
+			Console.WriteLine("{0}=== Location(s) by City/State ===", Environment.NewLine);
 			IEnumerable<Location> locs = locationSvc.GetByCityState("Tulare", "CA");
 			foreach (Location loc in locs)
 			{
@@ -28,18 +29,20 @@ namespace LocationByZip.DemoConsoleApp
 			}
 
 
-			//	Distance between two locations.
-			double distance = locationSvc.GetDistanceBetweenLocations("93401", "93446");
-			Console.WriteLine("93401 is {0:F1} miles from 93446", distance);
-			Console.WriteLine();
-
-
-			//	Other Locations within an X-mile radius of a specific location.
+			// Location(s) in Radius
+			Console.WriteLine("{0}=== Location(s) in Radius ===", Environment.NewLine);
 			IEnumerable<LocationInRadius> locsInRadius = locationSvc.GetLocationsInRadius("93401", 10.0);
 			foreach (Location locInRad in locsInRadius)
 			{
 				DisplayLocation(locInRad);
 			}
+
+
+			//	Distance between two locations.
+			Console.WriteLine("{0}=== Distance between two ZIP Codes ===", Environment.NewLine);
+			double distance = locationSvc.GetDistanceBetweenLocations("93401", "93446");
+			Console.WriteLine("93401 is {0:F1} miles from 93446", distance);
+			Console.WriteLine();
 
 
 			Console.ReadLine();
